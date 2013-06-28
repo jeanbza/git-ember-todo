92770ea8ab0ccceb7d4cecb6516b707e561c938f
jadekler <jadekler@gmail.com>
2013-06-28 13:44:48 -0400

    Initial template fix with correct, ember.js working code

--- index.php
+++ index.php
@@ -3,47 +3,35 @@
 	<script type="text/javascript" src="js/jquery/jquery-1.9.1.min.js"></script>
 	<script type="text/javascript" src="js/jquery-ui/ui/jquery-ui.js"></script>
 	<script type="text/javascript" src="js/bootstrap/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="js/angular/angular.min.js"></script>
+	<script type="text/javascript" src="js/ember/handlebars-1.0.0-rc.4.js"></script>
+	<script type="text/javascript" src="js/ember/ember-1.0.0-rc.6.min.js"></script>
 	<script type="text/javascript" src="js/app.js"></script>
 
 	<link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap-responsive.min.css">
 	<link rel="stylesheet" type="text/css" href="css/app.css">
 </head>
-<body ng-app>
-	<div ng-controller="demoControl" class="container-fluid">
-		<div class="row-fluid">
-			<h1>This is a demo <small>of Angular.js usage</small></h1>
-			<span>
-				Explanation
-			</span>
+<body>
+	<script type="text/x-handlebars">
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<div class="span12">
+					<legend>Create A TODO</legend>
+					<input class="todo-input" type="text" placeholder="Add a TODO">
+					<button class='btn btn-primary btn-submit-todo'><i class='icon-ok icon-white'></i></button>
+				</div>
+			</div>
+			{{#each someItem in someItems}}
+				<div class="row-fluid">
+					<div class="span6">
+						{{someItem}}
+					</div>
+					<div class="span6">
+						{{someItem}}
+					</div>
+				</div>
+			{{/each}}
 		</div>
-		<div class="row-fluid addNameArea">
-			<form ng-submit="addName()">
-				<fieldset>
-					<label><strong>Add A Name Here</strong></label>
-					<input ng-model="nameInput" type="text" placeholder="Add A Name Here">
-					<button type="submit" class="btn btn-primary">Submit</button>
-					<button ng-click="clearList()" type="button" class="btn">Clear List</button>
-				</fieldset>
-			</form>
-		</div>
-		<div class="row-fluid">
-			<table class="table">
-				<thead>
-					<tr>
-						<td>#</td>
-						<td>Name</td>
-					</tr>
-				</thead>
-				<tbody>
-					<tr ng-repeat="listItem in someList">
-						<td>{{listItem.num}}</td>
-						<td>{{listItem.name}}</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-	</div>
+	</script>
 </body>
 </html>
