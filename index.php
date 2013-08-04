8df954c1144716452086c1d50bd547a98bd1d9a8
jadekler <jadekler@gmail.com>
2013-08-04 13:19:47 -0400

    Fairly large revamp, switched to proper MVC Ember.Data usage to properly show model-controller-view interaction. Using fixture adapter in this example.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<script type="text/javascript" src="js/bootstrap/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/ember/handlebars-1.0.0-rc.4.js"></script>
 	<script type="text/javascript" src="js/ember/ember-1.0.0-rc.6.js"></script>
+	<script type="text/javascript" src="js/ember/ember-data-latest.min.js"></script>
 	<script type="text/javascript" src="js/app.js"></script>
 
 	<link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.min.css">
@@ -16,48 +17,41 @@
 		<div class="container-fluid">
 			<div class="row-fluid">
 				<div class="span12">
-					<legend>
-						<span>Create A TODO</span>
-						<span><small>Note: double click to edit</small></span>
-					</legend>
 					{{view App.InputField action="addTodo" class="todo-input" placeholder="Enter a todo" valueBinding="todoInput"}}
 					<button {{action addTodo}} class="btn btn-primary btn-submit-todo">
 						<i class="icon-ok icon-white"></i>
 					</button>
 				</div>
 			</div>
-
-			{{#each todoItem in App.TodoItems.items}}
-				{{view todoItem}}
+			{{#each todo in todos}}
+				{{view "App.TodoItemView"}}
 			{{/each}}
 		</div>
 	</script>
 
-	<script type="text/x-handlebars" data-template-name="todo-item">
-		<div class="span3 leftmost-span">
-			{{#view todoItem.title}}
-				{{#if todoItem.title.isEditing}}
-					{{view App.InputField valueBinding="todoItem.title.textContent" class="edit-todo-input"}}
-				{{else}}
-					{{todoItem.title.textContent}}
-				{{/if}}
+	<script type="text/x-handlebars" data-template-name="todo-item-template">
+		<div class="row-fluid tall-row">
+			{{#view "App.TodoDetailView"}}
+				<div class="span3 left-item">
+					{{#if view.isEditing}}
+						{{view App.InputField valueBinding="todo.title" class="edit-todo-input"}}
+					{{else}}
+						{{todo.title}}
+					{{/if}}
+				</div>
 			{{/view}}
-		</div>
-
-		<div class="span8">
-			{{#view todoItem.content}}
-				{{#if todoItem.content.isEditing}}
-					{{view App.InputField valueBinding="todoItem.content.textContent" class="edit-todo-input"}}
-				{{else}}
-					{{todoItem.content.textContent}}
-				{{/if}}
+			{{#view "App.TodoDetailView"}}
+				<div class="span8 middle-item">
+					{{#if view.isEditing}}
+						{{view App.InputField valueBinding="todo.content" class="edit-todo-input"}}
+					{{else}}
+						{{todo.content}}
+					{{/if}}
+				</div>
 			{{/view}}
-		</div>
-
-		<div class="span1 right-align">
-			<button {{action removeTodo todoItem}}>
-				<i class="icon-trash"></i>
-			</button>
+			<div class="span1 right-item">
+				<button {{action removeTodo todo}}><i class="icon-trash"></i></button>
+			</div>
 		</div>
 	</script>
 </body>
--- js/app.js
+++ js/app.js
@@ -2,59 +2,44 @@
 
 window.App = Ember.Application.create();
 
-// VIEW
+// ROUTE
 
-/**
- * This is a todo item view
- * @param {string}     templateName	Tells ember which template to render into
- * @param {TodoDetail} title        The title (view) of our todo item
- * @param {TodoDetail} content      The content (view) of our todo item
- * @param {array}      classNames   An array of strings that ember will assign as classnames to this view's DOM element
- */
-App.TodoItem = Ember.View.extend({
-	templateName: 'todo-item',
-
-	title: null,
-	content: null,
-	classNames: ["row-fluid", "tall-row", "todoItem"],
-
-	didInsertElement: function() {
-		this.attachPopover();
+App.IndexRoute = Ember.Route.extend({
+	model: function() {
+		return App.Todo.find();
 	},
 
-	titleChanged: function() {
-		this.attachPopover();
-	}.observes('title.textContent'),
-
-	contentChanged: function() {
-		this.attachPopover();
-	}.observes('content.textContent'),
-
-	attachPopover: function() {
-		this.$().popover('destroy');
-		this.$().popover({
-			html: true,
-			title: self.title,
-			placement: "left",
-			trigger: "hover",
-			title: this.title.textContent,
-			content: this.content.textContent
-		});
+	setupController: function(controller, model) {
+		this.controllerFor('application').set('todos', model);
 	}
 });
 
-/**
- * An individual detail about a todo item. We want this because different parts of our todo item will act differently 
- * (e.g. double clicking delete icon is not the same as clicking the todo title) and independently (e.g. I should
- * be able to double click and edit the title independently of the content)
- * @param {string}  textContent The text content of this part of the todo item (e.g. title's text, content's text)
- * @param {string}  tagName     The tagname ember will assign to this view
- * @param {boolean} isEditing   A boolean in charge of keeping track of whether this view is being edited (by user double 
- *                              click) or not
- */
-App.TodoDetail = Ember.View.extend({
-	textContent: "",
-	tagName: "div",
+// MODEL
+
+App.Store = DS.Store.extend({
+	adapter: DS.FixtureAdapter.create()
+});
+
+App.Todo = DS.Model.extend({
+	title: DS.attr('string'),
+	content: DS.attr('string')
+});
+
+App.Todo.FIXTURES = [
+	{id: 1, title: "Go to the grocery store", content: "For bread, butter, milk, and other stuff"},
+	{id: 2, title: "Milk the cow", content: "Because we live on a farm I suppose"},
+	{id: 3, title: "Collect honey", content: "I think that's also something people on farms do"},
+	{id: 4, title: "Fetch kids from school", content: "Carpooling with David from Finance"},
+	{id: 5, title: "I don't know", content: "What people put on lists like these"}
+];
+
+// VIEW
+
+App.TodoItemView = Ember.View.extend({
+	templateName: "todo-item-template"
+});
+
+App.TodoDetailView = Ember.View.extend({
 	isEditing: false,
 
 	doubleClick: function() {
@@ -80,48 +65,19 @@ App.InputField = Ember.TextField.extend({
 
 // CONTROLLER
 
-/**
- * Controller which is mostly in charge of modifying the model
- * @param {string} todoInput The valueBinding location which the giant Add A Todo input box targets
- */
 App.ApplicationController = Ember.Controller.extend({
 	todoInput: "",
+	todos: [],
 
 	addTodo: function() {
-		App.TodoItems.addTodo(this.get("todoInput"), "No content.");
-		this.set("todoInput", "");
+		App.Todo.createRecord({
+			title: this.get('todoInput'),
+			content: "Temp content"
+		});
+		this.set('todoInput', "");
 	},
 
-	modifyTodo: function(todoItem) {
-		todoItem.set("modifiable", !todoItem.modifiable);
-	},
-
-	removeTodo: function(todoItem) {
-		App.TodoItems.items.removeObject(todoItem);
-	}
-});
-
-// MODEL: Please note, in a real world application our model would probably not be represented as such - instead,
-// 		  we would use the DB and a DS.Store. For the sake of simplicity, we will skip that for now
-
-/**
- * This controller holds our todo item views and knows how to add more
- * @param {array} items Our 'model' - an array of views
- */
-App.TodoItems = Ember.ArrayController.create({
-	items: [],
-
-	init: function() {
-		for(x = 0; x < 5; x++) {
-			this.addTodo("TODO "+x, "Content "+x+" - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sit amet tempor sem.");
-		}
-	},
-
-	addTodo: function(title, content) {
-		var tempTitle = App.TodoDetail.create({textContent: title});
-		var tempContent = App.TodoDetail.create({textContent: content});
-		var tempTodo = App.TodoItem.create({title: tempTitle, content: tempContent});
-
-		this.items.pushObject(tempTodo);
+	removeTodo: function(todo) {
+		todo.deleteRecord();
 	}
 });
