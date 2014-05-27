64ace1fbda3ed157cbd1bf4786cbdbe07ccde9de
jadekler <jadekler@gmail.com>
2014-05-27 09:37:38 -0400

    Changed from tabs to spaces

--- index.php
+++ index.php
@@ -1,58 +1,58 @@
 <html>
 <head>
-	<script type="text/javascript" src="js/jquery/jquery-1.9.1.min.js"></script>
-	<script type="text/javascript" src="js/jquery-ui/ui/jquery-ui.js"></script>
-	<script type="text/javascript" src="js/bootstrap/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="js/ember/handlebars-1.0.0-rc.4.js"></script>
-	<script type="text/javascript" src="js/ember/ember-1.0.0-rc.6.js"></script>
-	<script type="text/javascript" src="js/ember/ember-data-latest.min.js"></script>
-	<script type="text/javascript" src="js/app.js"></script>
+    <script type="text/javascript" src="js/jquery/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="js/jquery-ui/ui/jquery-ui.js"></script>
+    <script type="text/javascript" src="js/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/ember/handlebars-1.0.0-rc.4.js"></script>
+    <script type="text/javascript" src="js/ember/ember-1.0.0-rc.6.js"></script>
+    <script type="text/javascript" src="js/ember/ember-data-latest.min.js"></script>
+    <script type="text/javascript" src="js/app.js"></script>
 
-	<link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap-responsive.min.css">
-	<link rel="stylesheet" type="text/css" href="css/app.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap-responsive.min.css">
+    <link rel="stylesheet" type="text/css" href="css/app.css">
 </head>
 <body>
-	<script type="text/x-handlebars">
-		<div class="container-fluid">
-			<div class="row-fluid">
-				<div class="span12">
-					{{view App.InputField action="addTodo" class="todo-input" placeholder="Enter a todo" valueBinding="todoInput"}}
-					<button {{action addTodo}} class="btn btn-primary btn-submit-todo">
-						<i class="icon-ok icon-white"></i>
-					</button>
-				</div>
-			</div>
-			{{#each todo in todos}}
-				{{view "App.TodoItemView"}}
-			{{/each}}
-		</div>
-	</script>
+    <script type="text/x-handlebars">
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span12">
+                    {{view App.InputField action="addTodo" class="todo-input" placeholder="Enter a todo" valueBinding="todoInput"}}
+                    <button {{action addTodo}} class="btn btn-primary btn-submit-todo">
+                        <i class="icon-ok icon-white"></i>
+                    </button>
+                </div>
+            </div>
+            {{#each todo in todos}}
+                {{view "App.TodoItemView"}}
+            {{/each}}
+        </div>
+    </script>
 
-	<script type="text/x-handlebars" data-template-name="todo-item-template">
-		<div class="row-fluid tall-row">
-			{{#view "App.TodoDetailView"}}
-				<div class="span3 left-item">
-					{{#if view.isEditing}}
-						{{view App.InputField valueBinding="todo.title" class="edit-todo-input"}}
-					{{else}}
-						{{todo.title}}
-					{{/if}}
-				</div>
-			{{/view}}
-			{{#view "App.TodoDetailView"}}
-				<div class="span8 middle-item">
-					{{#if view.isEditing}}
-						{{view App.InputField valueBinding="todo.content" class="edit-todo-input"}}
-					{{else}}
-						{{todo.content}}
-					{{/if}}
-				</div>
-			{{/view}}
-			<div class="span1 right-item">
-				<button {{action removeTodo todo}}><i class="icon-trash"></i></button>
-			</div>
-		</div>
-	</script>
+    <script type="text/x-handlebars" data-template-name="todo-item-template">
+        <div class="row-fluid tall-row">
+            {{#view "App.TodoDetailView"}}
+                <div class="span3 left-item">
+                    {{#if view.isEditing}}
+                        {{view App.InputField valueBinding="todo.title" class="edit-todo-input"}}
+                    {{else}}
+                        {{todo.title}}
+                    {{/if}}
+                </div>
+            {{/view}}
+            {{#view "App.TodoDetailView"}}
+                <div class="span8 middle-item">
+                    {{#if view.isEditing}}
+                        {{view App.InputField valueBinding="todo.content" class="edit-todo-input"}}
+                    {{else}}
+                        {{todo.content}}
+                    {{/if}}
+                </div>
+            {{/view}}
+            <div class="span1 right-item">
+                <button {{action removeTodo todo}}><i class="icon-trash"></i></button>
+            </div>
+        </div>
+    </script>
 </body>
 </html>
--- js/app.js
+++ js/app.js
@@ -5,79 +5,79 @@ window.App = Ember.Application.create();
 // ROUTE
 
 App.IndexRoute = Ember.Route.extend({
-	model: function() {
-		return App.Todo.find();
-	},
+    model: function() {
+        return App.Todo.find();
+    },
 
-	setupController: function(controller, model) {
-		this.controllerFor('application').set('todos', model);
-	}
+    setupController: function(controller, model) {
+        this.controllerFor('application').set('todos', model);
+    }
 });
 
 // MODEL
 
 App.Store = DS.Store.extend({
-	adapter: DS.FixtureAdapter.create()
+    adapter: DS.FixtureAdapter.create()
 });
 
 App.Todo = DS.Model.extend({
-	title: DS.attr('string'),
-	content: DS.attr('string')
+    title: DS.attr('string'),
+    content: DS.attr('string')
 });
 
 App.Todo.FIXTURES = [
-	{id: 1, title: "Go to the grocery store", content: "For bread, butter, milk, and other stuff"},
-	{id: 2, title: "Milk the cow", content: "Because we live on a farm I suppose"},
-	{id: 3, title: "Collect honey", content: "I think that's also something people on farms do"},
-	{id: 4, title: "Fetch kids from school", content: "Carpooling with David from Finance"},
-	{id: 5, title: "I don't know", content: "What people put on lists like these"}
+    {id: 1, title: "Go to the grocery store", content: "For bread, butter, milk, and other stuff"},
+    {id: 2, title: "Milk the cow", content: "Because we live on a farm I suppose"},
+    {id: 3, title: "Collect honey", content: "I think that's also something people on farms do"},
+    {id: 4, title: "Fetch kids from school", content: "Carpooling with David from Finance"},
+    {id: 5, title: "I don't know", content: "What people put on lists like these"}
 ];
 
 // VIEW
 
 App.TodoItemView = Ember.View.extend({
-	templateName: "todo-item-template"
+    templateName: "todo-item-template"
 });
 
 App.TodoDetailView = Ember.View.extend({
-	isEditing: false,
+    isEditing: false,
 
-	doubleClick: function() {
-		this.set("isEditing", true);
-	},
+    doubleClick: function() {
+        this.set("isEditing", true);
+    },
 
-	keyUp: function(e) {
-		if(e.keyCode == 13) {
-			this.set("isEditing", false);
-		}
-	},
+    keyUp: function(e) {
+        if(e.keyCode == 13) {
+            this.set("isEditing", false);
+        }
+    },
 
-	focusOut: function() {
-		this.set("isEditing", false);
-	}
+    focusOut: function() {
+        this.set("isEditing", false);
+    }
 });
 
 App.InputField = Ember.TextField.extend({
-	didInsertElement: function() {
-		this.$().focus();
-	}
+    didInsertElement: function() {
+        this.$().focus();
+    }
 });
 
 // CONTROLLER
 
 App.ApplicationController = Ember.Controller.extend({
-	todoInput: "",
-	todos: [],
+    todoInput: "",
+    todos: [],
 
-	addTodo: function() {
-		App.Todo.createRecord({
-			title: this.get('todoInput'),
-			content: "Temp content"
-		});
-		this.set('todoInput', "");
-	},
+    addTodo: function() {
+        App.Todo.createRecord({
+            title: this.get('todoInput'),
+            content: "Temp content"
+        });
+        this.set('todoInput', "");
+    },
 
-	removeTodo: function(todo) {
-		todo.deleteRecord();
-	}
+    removeTodo: function(todo) {
+        todo.deleteRecord();
+    }
 });
